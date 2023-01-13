7b7cff707dcb9c25d953aca43f910e0fe41fad68
arafat_thabet <arafat.733011506@gmail.com>
2023-01-14 01:37:38 +0300

    fix

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,7 +1,7 @@
 @php
 $logo = 'img/logo.jfif';
 if(isset(\App\Models\Location::first()->logo) && !empty(\App\Models\Location::first()->logo )){
-    $logo = "storage/".(\App\Models\Location::first()->logo;
+    $logo = "storage/".\App\Models\Location::first()->logo;
 }
 @endphp
 <img src="{{ asset($logo)}}" alt="Logo" class="h-10">
