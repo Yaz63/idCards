b5079f4587530057b6544209e355cb68e83821f7
arafat_thabet <arafat.733011506@gmail.com>
2023-01-14 01:32:03 +0300

    update

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,4 +1,4 @@
 @php
-$logo = "storage/".\App\Models\Location::where('id', 1)->first()->logo ?? 'img/logo.jfif';
+$logo = "storage/".\App\Models\Location::first()->logo ?? 'img/logo.jfif';
 @endphp
 <img src="{{ asset($logo)}}" alt="Logo" class="h-10">
